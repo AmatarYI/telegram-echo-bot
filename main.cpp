3cfc935ec69ec140423059f7bda47cf9161b8ddd
AmatarYI <riyu.amataryi@gmail.com>
2020-07-27 00:35:14 +0300

    Added simple photo replying

--- main.cpp
+++ main.cpp
@@ -2,19 +2,27 @@
 
 void showMessageAndInfo(TgBot::Message::Ptr);
 void showMessage(TgBot::Message::Ptr);
+bool isPhoto(TgBot::Message::Ptr);
 
 int main() {
     TgBot::Bot bot("TOKEN");
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
         bot.getApi().sendMessage(message->chat->id, "Hi!\nI'm echo-bot written on C++\nAnd soon the Creator will kill me");
     });
+
     bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-
-        showMessageAndInfo(message);
-
         if (StringTools::startsWith(message->text, "/start")) {
             return;
         }
+        if(isPhoto(message))
+        {
+            bot.getApi().sendMessage(message->chat->id, "Your message is photo. Replying...");
+            bot.getApi().forwardMessage(message->chat->id,message->from->id, message->messageId,false);
+            showMessageAndInfo(message);
+            return;
+        }
+        showMessageAndInfo(message);
+
         bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
     });
     try {
@@ -32,12 +40,19 @@ int main() {
 
 void showMessageAndInfo(TgBot::Message::Ptr message){
     std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<< std::endl;
-    std::cout << "ID: " <<message->from->id << "\nUsername: " << message->from->username  << "\nFirst name: " << message->from->firstName
-              << "\nSecond name: " << message->from->lastName << std::endl;
-    showMessage(message);
+    std::cout << "ID: " <<message->from->id << "\nUsername: " << message->from->username  << "\nFirst name: "
+    << message->from->firstName << "\nSecond name: " << message->from->lastName << std::endl;
+    if(!isPhoto(message))
+        showMessage(message);
+    else
+        std::cout << "Photo ? Size: " <<message->photo.size() << std::endl;
     std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<< std::endl;
 }
 
 void showMessage(TgBot::Message::Ptr message){
     std::cout << "Message: " << message->text.c_str() << std::endl;
+}
+
+bool isPhoto(TgBot::Message::Ptr message) {
+    return message->photo.size()!=0;
 }
